25c826947a70b6b4065b9e1bbfa594229a11b5f2
sharad <sharad@gmail.com>
2021-12-21 16:53:19 +0530

    first commit

--- sample1.py
+++ sample1.py
@@ -3,10 +3,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 #driver = webdriver.Firefox(executable_path=r"C:\Users\Sharad\Desktop\drivers\geckodriver.exe")
 #driver = webdriver.Ie(executable_path=r"C:\Users\Sharad\Desktop\drivers\IEDriverServer.exe")
-driver.get("https://www.google.com/")
+driver.get("https://www.amazon.com/")
 print(driver.title)
 driver.find_element_by_xpath("//*[@id='nav-link-accountList']/div").click()
 time.sleep(5)
