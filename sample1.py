d93b1311e5f81ccae3aa41df39887aa5d4dd5e86
sharad <sharad@gmail.com>
2022-01-07 13:01:56 +0530

    first commit

--- sample1.py
+++ sample1.py
@@ -5,9 +5,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#driver = webdriver.Chrome()
+driver = webdriver.Chrome()
 
-driver = webdriver.Firefox()
+#driver = webdriver.Firefox()
 
 driver.get("https://www.amazon.com/")
 
